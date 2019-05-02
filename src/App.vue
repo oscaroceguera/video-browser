f595e35529b7e4d28127f97e3b7c63d1f0bb6b9a
oscar <oscaroceguera@gmail.com>
2019-05-02 00:22:01 -0600

    Thumbnail image reference

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <div>
-    <SearchBar></SearchBar>
+  <div class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <VideoList :videos="myListOfVideos" />
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   import SearchBar from './components/SearchBar'
+  import VideoList from './components/VideoList'
+
+  // v-on = @
+  // v-bind = :
+
+  const API_KEY = 'AIzaSyBSmJyowEg2kWa19Axp1kcflKncqAG_P24'
 
   export default {
     name: 'App',
     components: {
-      SearchBar
+      SearchBar: SearchBar,
+      VideoList: VideoList
+    },
+    data () {
+      return {
+        myListOfVideos: []
+      }
+    },
+    methods: {
+      onTermChange (searchTerm)  {
+        axios.get('https://www.googleapis.com/youtube/v3/search', {
+          params: {
+            key: API_KEY,
+            type: 'video',
+            part: 'snippet',
+            q: searchTerm
+          }
+        }).then(response => this.myListOfVideos = response.data.items)
+      }
     }
   }
 </script>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="list-group-item">
+    <img v-bind:src="video.snippet.thumbnails.default.url" />
+    {{video.snippet.title}}
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'VideoListItem',
+  props: ['video']
+}
+</script>
+
+<style>
+
+</style>
